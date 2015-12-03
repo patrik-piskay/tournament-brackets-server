b1d6ec69f31a44a277674fed61aefaeba6c084db
Patrik Piskay <piskay.patrik@gmail.com>
2015-12-03 17:08:22 +0100

    shuffle players, updating score

--- server/database.js
+++ server/database.js
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3';
 import crypto from 'crypto';
+import { shuffle } from 'lodash/collection';
 
 const db = new sqlite3.Database('brackets.db');
 
@@ -27,14 +28,19 @@ db.serialize(() => {
         tournament_id   INTEGER NOT NULL,
         player1_id      INTEGER DEFAULT NULL,
         player2_id      INTEGER DEFAULT NULL,
+        player1_score   INTEGER DEFAULT NULL,
+        player2_score   INTEGER DEFAULT NULL,
         next_round_id   TEXT DEFAULT NULL,
         played_at       TIMESTAMP DEFAULT NULL
     )`);
 
-    db.run(`INSERT INTO tournament (name) VALUES ('t1'), ('t2')`);
+    // db.run(`INSERT INTO tournament (name) VALUES ('t1'), ('t2')`);
     db.run(`INSERT INTO player (name, tournament_id) VALUES
         ('Pato', 1),
-        ('Majka', 1)
+        ('Majka', 1),
+        ('Rudo', 1),
+        ('Kika', 1),
+        ('Jozo', 1)
     `);
     // db.run(`INSERT INTO match (tournament_id, player1_id, player2_id, next_round_id) VALUES
     //     (1, null, 3, null),
@@ -83,14 +89,74 @@ export const generateMatches = (tournamentId, players, nextRoundId = null) => {
     }
 };
 
+const getMatch = (matchId, cb) => {
+    db.all(`SELECT *, datetime(played_at, 'localtime') as played_at FROM match WHERE id = ?`, matchId, (err, rows) => {
+        cb(rows.length, rows[0], err);
+    });
+};
+
+const sendWinnerToTheNextRound = (nextRoundId, winnerId, cb) => {
+    getMatch(nextRoundId, (exists, match, error) => {
+        if (exists) {
+            let slot = null;
+            if (match.player1_id === null) {
+                slot = 'player1_id';
+            } else if (match.player2_id === null) {
+                slot = 'player2_id';
+            } else {
+                cb(null, {
+                    err: 'Both players are already assigned to the match'
+                });
+                return;
+            }
+
+            db.run(`UPDATE match SET
+                        ${slot} = $winnerId
+                    WHERE id = $id`, {
+                        $id: nextRoundId,
+                        $winnerId: winnerId
+                    }, function(err) {
+                        if (!err) {
+                            cb(1);
+                        } else {
+                            cb(null, err);
+                        }
+                    }
+            );
+        } else {
+            // test this
+            cb(null, error);
+        }
+    });
+};
+
+const setTournamentAsFinished = (tournamentId, cb) => {
+    db.run(`UPDATE tournament SET
+                finished = 1
+            WHERE id = ?`, tournamentId, function(err) {
+                if (!err) {
+                    if (this.changes) {
+                        cb(1);
+                    } else {
+                        cb(null, {
+                            err: 'Tournament does not exists'
+                        });
+                    }
+                } else {
+                    cb(null, err);
+                }
+            }
+    );
+};
+
 export default {
     getTournaments: (cb) => {
-        db.all(`SELECT *, datetime(created_at, 'localtime') as created FROM tournament`, (err, rows) => {
+        db.all(`SELECT *, datetime(created_at, 'localtime') as created_at FROM tournament`, (err, rows) => {
             cb(rows, err);
         });
     },
 
-    getTournament: (id, cb) => {
+    getTournament: (tournamentId, cb) => {
         db.all(`
             SELECT tournament.*, datetime(created_at, 'localtime') as created_at,
                 match.*, datetime(played_at, 'localtime') as played_at,
@@ -99,20 +165,94 @@ export default {
             LEFT JOIN match on tournament.id = match.tournament_id
             LEFT JOIN player as player1 on match.player1_id = player1.id
             LEFT JOIN player as player2 on match.player2_id = player2.id
-            WHERE tournament.id = ?`, id, (err, rows) => {
-                cb(rows, err);
+            WHERE tournament.id = ?`, tournamentId, (err, rows) => {
+                if (rows) {
+                    const { tournament_id, name, created_at, finished } = rows[0];
+
+                    const tournament = {
+                        id: tournament_id,
+                        name,
+                        created_at,
+                        finished
+                    };
+
+                    const matches = rows.map((match) => {
+                        const {
+                            id, player1, player2, player1_score, player2_score,
+                            next_round_id, played_at
+                        } = match;
+
+                        return {
+                            id,
+                            player1,
+                            player2,
+                            player1_score,
+                            player2_score,
+                            nextRoundId: next_round_id,
+                            playedAt: played_at
+                        };
+                    });
+
+                    cb({
+                        tournament,
+                        matches
+                    });
+                } else {
+                    cb(null, err);
+                }
             }
         );
     },
 
-    insertTournament: (name, players) => {
+    insertTournament: (name, players, cb) => {
         db.run(`INSERT INTO tournament (name) VALUES (?)`, [name], function(err) {
             if (!err && this.lastID) {
-                const matches = generateMatches(this.lastID, players);
+                const matches = generateMatches(this.lastID, shuffle(players));
 
                 matches.forEach((match) => {
                     insertMatch(match);
                 });
+
+                cb(this.lastID);
+            } else {
+                cb(null, err);
+            }
+        });
+    },
+
+    getMatch,
+
+    updateScore: (matchId, player1Score, player2Score, cb) => {
+        getMatch(matchId, (exists, match, err) => {
+            if (exists) {
+                db.run(`UPDATE match SET
+                            player1_score = $player1Score,
+                            player2_score = $player2Score,
+                            played_at = datetime('now')
+                        WHERE id = $id`, {
+                            $id: matchId,
+                            $player1Score: player1Score,
+                            $player2Score: player2Score
+                        }, function(error) {
+                            if (!err) {
+                                if (match.next_round_id) {
+                                    const winnerId = player1Score > player2Score ? match.player1_id : match.player2_id;
+                                    sendWinnerToTheNextRound(match.next_round_id, winnerId, (success, _error) => {
+                                        cb(success, _error);
+                                    });
+                                } else {
+                                    setTournamentAsFinished(match.tournament_id, (success, _error) => {
+                                        cb(success, _error);
+                                    });
+                                }
+                            } else {
+                                cb(null, error);
+                            }
+                        }
+                );
+            } else {
+                // test this
+                cb(null, err);
             }
         });
     }
--- server/server.js
+++ server/server.js
@@ -13,21 +13,23 @@ app.get('/', function(req, res) {
 
 app.get('/get-tournaments', function(req, res) {
     db.getTournaments((tournaments, err) => {
-        res.json({
-            tournaments,
-            err
-        });
+        if (!err) {
+            res.status(200).json(tournaments);
+        } else {
+            res.status(500).json(err);
+        }
     });
 });
 
 app.get('/get-tournament/:id', function(req, res) {
-    const id = req.params.id;
+    const id = parseInt(req.params.id, 10);
 
     db.getTournament(id, (tournament, err) => {
-        res.json({
-            tournament,
-            err
-        });
+        if (!err) {
+            res.status(200).json(tournament);
+        } else {
+            res.status(500).json(err);
+        }
     });
 });
 
@@ -36,14 +38,49 @@ app.post('/create-tournament', function(req, res) {
     const players = req.body.players;
 
     if (name && players) {
-        db.insertTournament(name, JSON.parse(players));
+        db.insertTournament(name, JSON.parse(players), (tournamentId, err) => {
+            if (!err) {
+                res.status(200).json({ tournamentId });
+            } else {
+                res.status(500).json(err);
+            }
+        });
     } else {
-        
+        res.status(400);
     }
 });
 
-app.post('/update-score/:matchId', function(req, res) {
-    res.send('Hello World!');
+app.get('/get-match/:matchId', function(req, res) {
+    const matchId = req.params.matchId;
+
+    db.getMatch(matchId, (exists, match, err) => {
+        if (err) {
+            res.status(500).json(err);
+        } else if (!exists) {
+            res.status(404).json({ err: 'Not found' });
+        } else {
+            res.status(200).json(match);
+        }
+    });
+});
+
+app.put('/update-score/:matchId', function(req, res) {
+    const matchId = req.params.matchId;
+    const player1Score = parseInt(req.body.player1_score, 10);
+    const player2Score = parseInt(req.body.player2_score, 10);
+
+    if (player1Score === player2Score) {
+        // match in tournament can not end with a draw
+        res.status(400);
+    }
+
+    db.updateScore(matchId, player1Score, player2Score, (result, err) => {
+        if (!err) {
+            res.status(200).json(result);
+        } else {
+            res.status(500).json(err);
+        }
+    });
 });
 
 const server = app.listen(3000, function() {
